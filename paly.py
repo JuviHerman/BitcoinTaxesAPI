e0216eab1e55c290584883e9af3a8575c3b32569
Yuval Herman <yuvalherm@gmail.com>
2019-02-04 23:51:29 +0200

    Update paly.py

--- paly.py
+++ paly.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import requests
-headers  = {"Content-Type": "application/json","UserAgent": "RonG poloniex"}
-params = {"taxyear" : '2017',"start" : '0', "limit": '1000'}
-Authorization = ('2d1242924df291ca', '06486dd0306a07b0409468e6af1fdc77')
+
+application = "XXXXXX" #name of project
+headers  = {"Content-Type": "application/json","UserAgent": application }
+
+Year = '2017'
+params = {"taxyear" : Year,"start" : '0', "limit": '1000'}
+
+apikey = 'abcd'
+apisecret = 'kjshdgfkihsgdf'
+Authorization = (apikey, apisecret)
+
 resp = requests.get('https://api.bitcoin.tax/v1/transactions/',headers = headers,params=params, auth = Authorization )
+
 print(resp)
 if resp.status_code != 200:
     # This means something went wrong.
